2a4f919eb586a4a5482af7dc6726704993dfd607
Maheshkumar <callmeelprofessor@gmail.com>
2021-11-24 15:27:11 +0530

    Complete 13th exercise assignments

--- spec/13_input_output_spec.rb
+++ spec/13_input_output_spec.rb
@@ -175,9 +175,15 @@ describe NumberGame do
     # Write a test for the following context.
     context 'when user inputs two incorrect values, then a valid input' do
       before do
+        first_incorrect_value = 'first'
+        second_incorrect_value = 'second'
+        correct_value = '8'
+        allow(game_loop).to receive(:player_input).and_return(first_incorrect_value, second_incorrect_value, correct_value)
       end
 
-      xit 'completes loop and displays error message twice' do
+      it 'completes loop and displays error message twice' do
+        expect(game_loop).to receive(:puts).with('Input error!').twice
+        game_loop.player_turn
       end
     end
   end
@@ -208,8 +214,9 @@ describe NumberGame do
     # Create a new instance of NumberGame, with specific values for @solution,
     # @guess, and @count
     context 'when count is 2-3' do
+      subject(:game) { NumberGame.new('5', 5, 3) }
       # remove the 'x' before running this test
-      xit 'outputs correct phrase' do
+      it 'outputs correct phrase' do
         congrats_phrase = "Congratulations! You picked the random number in 3 guesses!\n"
         expect { game.final_message }.to output(congrats_phrase).to_stdout
       end
@@ -219,8 +226,11 @@ describe NumberGame do
 
     # Write a test for the following context.
     context 'when count is 4 and over' do
+      subject(:game) { NumberGame.new('5', 5, 7) }
       # remove the 'x' before running this test
-      xit 'outputs correct phrase' do
+      it 'outputs correct phrase' do
+        hard_phrase = "That was hard. It took you 7 guesses!\n"
+        expect { game.final_message }.to output(hard_phrase).to_stdout
       end
     end
   end
